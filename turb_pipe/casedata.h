75841a3a7cf1c83fdbe73bd2b7ac60ed6fd343a8
tonyzahtila <tonyzahtila@gmail.com>
2021-04-08 15:45:38 +1000

    latest

---
+++ turb_pipe/casedata.h
@@ -0,0 +1,8 @@
+
+
+#define ZLENGTH  (2.*PI)  /* diameters  */
+#define RE_TAU 180.0
+#define INTP_NUMZ       75
+#define INTP_NUMR       75
+#define INTP_NTHETA     25
+#define INTP_TOTP  INTP_NUMZ*INTP_NUMR*INTP_NTHETA
